0ef29fa6ba2ef366547e77930ca6325f0a4c14c5
henrycchgithub <120686100+henrycchgithub@users.noreply.github.com>
2024-02-28 14:32:56 -0700

    Configure DB & Redis variables

--- config/database.php
+++ config/database.php
@@ -141,7 +141,7 @@ return [
             'scheme' => 'tls',
             'url' => env('REDIS_URL'),
             'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAMEa'),
+            'username' => env('REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD'),
             'port' => env('AZURE_REDIS_PORT', '6379'),
             'database' => env('AZURE_REDIS_DATABASE', '1'),
